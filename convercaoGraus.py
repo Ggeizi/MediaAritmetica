2625c578cf7f94fdc8ce6c90f12e5f53545d9d8d
Geiziane Rocha <geizirocha586@gmail.com>
2025-11-06 16:57:19 -0300

    Add temperature conversion from Celsius to Fahrenheit

---
+++ convercaoGraus.py
@@ -0,0 +1,5 @@
+grausCelsius = (input (" Digite o valor da temperatura em graus Celsius: "))
+graus = int(grausCelsius)
+
+fahrenheit = (graus * 9 / 5) + 32
+print (" A temperatura de ", grausCelsius, " C° é igual a ", fahrenheit," F°  ")
