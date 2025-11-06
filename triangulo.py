abbfcdc5fff0d4db3d1251cb1a451bf5bb749aec
Geiziane Rocha <geizirocha586@gmail.com>
2025-11-06 16:55:43 -0300

    Add triangle area calculation in triangulo.py

---
+++ triangulo.py
@@ -0,0 +1,9 @@
+baseR = (input (" Digite o valor da base do triângulo: "))
+base = int(baseR)
+alturaR = (input (" Digite o valor da altura do triângulo: "))
+altura = int(alturaR)
+
+areaRetangulo = base * altura
+
+print (" A área do retângulo é ", areaRetangulo)
+
